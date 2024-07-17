9295a6f32bece32a42eda18c702e39acccc6f287
BlackHole00 <lastapocalypse9@gmail.com>
2024-07-17 16:14:35 +0200

    STD: Removed allocating using Context (since there are now multiple allocators in the context it is no longer clear which one is used)

--- modules/std/include/std/lang/mem.h
+++ modules/std/include/std/lang/mem.h
@@ -5,11 +5,9 @@
 #include <std/runtime/allocator.h>
 
 #define new(...) (STD_GET_ALL_BUT_LAST_VA_ARGS_WITH_COMMAS(__VA_ARGS__)*)_Generic(STD_GET_LAST_VA_ARGS(__VA_ARGS__), \
-	Allocator: allocator_alloc_single, \
-	Context*:  context_alloc_single \
+	Allocator: allocator_alloc_single \
 )(STD_GET_LAST_VA_ARGS(__VA_ARGS__), sizeof(STD_GET_ALL_BUT_LAST_VA_ARGS_WITH_COMMAS(__VA_ARGS__)))
 #define delete(...) _Generic(STD_GET_LAST_VA_ARGS(__VA_ARGS__), \
-	Allocator: allocator_dealloc_single, \
-	Context*:  context_dealloc_single \
+	Allocator: allocator_dealloc_single \
 )(STD_GET_LAST_VA_ARGS(__VA_ARGS__), STD_GET_ALL_BUT_LAST_VA_ARGS_WITH_COMMAS(__VA_ARGS__))
 
--- modules/std/include/std/runtime/context.h
+++ modules/std/include/std/runtime/context.h
@@ -12,36 +12,6 @@ typedef struct {
 	Logger logger;
 } Context;
 
-static inline Slice(byte) context_alloc(Context* context, usize size) {
-	Allocator allocator = context->allocator;
-	return allocator.vtable->alloc(allocator.allocator_data, size, true);
-}
-
-static inline void* context_alloc_single(Context* context, usize size) {
-	Allocator allocator = context->allocator;
-	return allocator_alloc_single(allocator, size);
-}
-
-static inline void context_dealloc(Context* context, Slice(byte) slice) {
-	Allocator allocator = context->allocator;
-	allocator.vtable->dealloc(allocator.allocator_data, slice);
-}
-
-static inline void context_dealloc_single(Context* context, void* ptr) {
-	Allocator allocator = context->allocator;
-	allocator_dealloc_single(allocator, ptr);
-}
-
-static inline Slice(byte) context_realloc(Context* context, Slice(byte) slice, usize size) {
-	Allocator allocator = context->allocator;
-	return allocator.vtable->realloc(allocator.allocator_data, slice, size, true);
-}
-
-static inline void context_dealloc_all(Context* context) {
-	Allocator allocator = context->allocator;
-	allocator.vtable->dealloc_all(allocator.allocator_data);
-}
-
 #define context_log(...)   context_log_ex   (sourcecodelocation_current(), __VA_ARGS__)
 #define context_fatal(...) context_fatal_ex (sourcecodelocation_current(), __VA_ARGS__)
 #define context_error(...) context_error_ex (sourcecodelocation_current(), __VA_ARGS__)
